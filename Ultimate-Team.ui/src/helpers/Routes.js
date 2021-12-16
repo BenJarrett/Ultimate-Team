7bcfcb25b3c1374ee9d0726b171171df1adec364
BenJarrett <b.cl.jarrett@gmail.com>
2021-12-16 13:38:37 -0600

    front end setup. MAKE TEMPLATE FROM THIS!

--- Ultimate-Team.ui/src/helpers/Routes.js
+++ Ultimate-Team.ui/src/helpers/Routes.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from '../views/LandingPage';
 
 export default function Routes() {
   return (
     <div>
+      <Switch>
+        <Route path='/'component={() => <LandingPage />} />
+
+      </Switch>
     </div>
   );
 }
---
+++ Ultimate-Team.ui/src/views/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className='landing'>
+      <h1 className='landingText'>Welcome to Ultimate Team</h1>
+      <h4 className='landingText'>Begin Collecting!</h4>
+      <br /><br />
+    </div>
+  );
+}
+
+export default LandingPage;
